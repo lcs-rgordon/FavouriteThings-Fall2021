2445edbad1023508281496b071cd28388170665e
Russell Gordon <rgordon@lcs.on.ca>
2021-10-24 20:31:53 -0400

    Added a list view to serve as the app landing page.

---
+++ FavouriteThings/Views/FavouriteThingsListView.swift
@@ -0,0 +1,24 @@
+//
+//  FavouriteThingsListView.swift
+//  FavouriteThings
+//
+//  Created by Russell Gordon on 2021-10-24.
+//
+
+import SwiftUI
+
+struct FavouriteThingsListView: View {
+    var body: some View {
+        List {
+            Text("Piper")
+            Text("Cheesecake")
+            Text("Blue Jays")
+        }
+    }
+}
+
+struct FavouriteThingsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavouriteThingsListView()
+    }
+}
