c72bf965e53f576e2bc17ac1dded082e99533cad
Russell Gordon <rgordon@lcs.on.ca>
2022-11-06 07:47:28 -0500

    Adjusted list view to use explicit parameters and arguments for NavigationLink structure, rather than a trailing closure.

--- FavouriteThings/Views/FavouriteThingsListView.swift
+++ FavouriteThings/Views/FavouriteThingsListView.swift
@@ -11,30 +11,41 @@ struct FavouriteThingsListView: View {
     var body: some View {
         List {
             
-            NavigationLink(destination: BlueJaysView()) {
+            NavigationLink(destination: {
+                BlueJaysView()
+            }, label: {
                 Text("Blue Jays")
-            }
+            })
 
-            NavigationLink(destination: CheesecakeView()) {
+            NavigationLink(destination: {
+                CheesecakeView()
+            }, label: {
                 Text("Cheesecake")
-            }
+            })
 
-            NavigationLink(destination: ClaireView()) {
+            NavigationLink(destination: {
+                ClaireView()
+            }, label: {
                 Text("Claire")
-            }
+            })
 
-            NavigationLink(destination: JenView()) {
+            NavigationLink(destination: {
+                JenView()
+            }, label: {
                 Text("Jen")
-            }
+            })
 
-            NavigationLink(destination: LasagnaView()) {
+            NavigationLink(destination: {
+                LasagnaView()
+            }, label: {
                 Text("Lasagna")
-            }
-            
-            NavigationLink(destination: PiperView()) {
-                Text("Piper")
-            }
+            })
 
+            NavigationLink(destination: {
+                PiperView()
+            }, label: {
+                Text("Piper")
+            })
             
         }
         .navigationTitle("My Favourite Things")
